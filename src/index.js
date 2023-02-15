4c5e5f1f4b16c5b72af88a887a788c77176b4b4c
Marisol Ospina <mov281215@gmail.com>
2023-02-15 08:27:50 -0500

    Subir el proyecto a github

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import validator from './validator.js';
+
+console.log(validator);
+alert('Hola mundo')
+
+function capturarDatos(){
+    const nombre=document.getElementById("inputNombre" ).value
+    const tarjeta=document.getElementById("inputNumero" ).value
+    const isArray= tarjeta.split('')
+    const tarjetaReverse = tarjeta.split('').reverse()/Imag/logoVisa.png
+    const posicion= tarjetaReverse
+    for (let i  = 0;; ) {
+        n += i;
+    
+      }
+
+ 
+    console.log(nombre)
+    console.log(tarjeta)
+    console.log(isArray)
+    console.log(tarjetaReverse)
+      
+}
+
+window.capturarDatos = capturarDatos;
